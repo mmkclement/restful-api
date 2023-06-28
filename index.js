ca2b47cfe8f0d52180fa8b3fa4d6ed91c8c826ef
mmkclement <maungmaungkha18290@gmail.com>
2023-06-28 11:44:58 +0200

    api courses finished

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get("/", (req, res) => {
   res.send("hello toxic world");
 });
 
+app.get("/api/courses", (req, res) => {
+  res.send([1,2,3]);
+});
+
 app.listen(3000, () => console.log("Listening on port 300"));
