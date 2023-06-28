cb47e377943bb2c49ecfba50d641317f61ebf9b1
mmkclement <maungmaungkha18290@gmail.com>
2023-06-28 14:12:54 +0200

    parameter work well

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ app.get("/api/courses", (req, res) => {
   res.send([1,2,3]);
 });
 
+app.get("/api/courses/:id" , (req,res) => {
+  res.send(req.params.id);
+});
 
 //PÖRT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
